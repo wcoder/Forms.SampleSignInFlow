f1e7c66838d51733144c2e7f669a1de29d4f44b2
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-10-22 13:38:20 +0300

    Fixed constants

--- src/LoginNavigationExample/LoginNavigationExample/Constants.cs
+++ src/LoginNavigationExample/LoginNavigationExample/Constants.cs
@@ -6,7 +6,6 @@
 
 		public static string AppId = "XXXXXXXXXXXXXXXXX";
 		public static string AppSecretId = "XXXXXXXXXXXXXXXXXXXXX";
-		public static string ExtendedPermissions = "wl.basic";
 		public static string ExtendedPermissions = "wl.basic"; // help: https://msdn.microsoft.com/en-us/library/office/dn631845.aspx
 		public static string AuthorizeUrl = "https://login.live.com/oauth20_authorize.srf";
 		public static string RedirectUrl = "https://login.live.com/oauth20_desktop.srf";
