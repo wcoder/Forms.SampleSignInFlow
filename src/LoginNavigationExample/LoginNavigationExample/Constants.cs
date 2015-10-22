e667b4e0ab734ca7f5f35714bf89aec34e3173c6
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-10-22 13:37:51 +0300

    Added help link

--- src/LoginNavigationExample/LoginNavigationExample/Constants.cs
+++ src/LoginNavigationExample/LoginNavigationExample/Constants.cs
@@ -7,6 +7,7 @@
 		public static string AppId = "XXXXXXXXXXXXXXXXX";
 		public static string AppSecretId = "XXXXXXXXXXXXXXXXXXXXX";
 		public static string ExtendedPermissions = "wl.basic";
+		public static string ExtendedPermissions = "wl.basic"; // help: https://msdn.microsoft.com/en-us/library/office/dn631845.aspx
 		public static string AuthorizeUrl = "https://login.live.com/oauth20_authorize.srf";
 		public static string RedirectUrl = "https://login.live.com/oauth20_desktop.srf";
 		public static string TokenUrl = "https://login.live.com/oauth20_token.srf";
